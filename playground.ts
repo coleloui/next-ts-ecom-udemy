d312fb0895bc68b4495c36f7a873c9afe8ebd776
Louis <coleloui18@gmail.com>
2021-11-14 11:15:43 -0800

    type and interface information

--- playground.ts
+++ playground.ts
@@ -1,3 +1,40 @@
+//ts recommends using interfaces over types
+
+// describe data structure in a more natural way
+interface Person {
+	name: string;
+	age: number;
+}
+
+// type aliases
+// to describe function types
+// type Person = {
+//     name: string,
+//     age: number
+// }
+
 export default function play() {
 	console.log('hello world');
+	const name = 'Louis';
+	const age = 32;
+
+	const person = {
+		name: 'Shannon',
+		age: 30,
+	};
+
+	function logPersonInfo(personName: string, personAge: number) {
+		const info = `Name: ${personName} age: ${personAge}`;
+		console.log(info);
+		return info;
+	}
+
+	function logPersonInfo2(person: Person) {
+		const info = `Name: ${person.name} age: ${person.age}`;
+		console.log(info);
+		return info;
+	}
+
+	logPersonInfo(name, age);
+	logPersonInfo2(person);
 }
